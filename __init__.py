28808cafceb7459de1b289ce20aa8905f27db4c6
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-06-11 15:06:04 +0100

    Rewrite to use tweepy

--- __init__.py
+++ __init__.py
@@ -1,14 +1,52 @@
+import json
 import logging
-import aiohttp
-import asyncio
-import math
-from aioauth_client import TwitterClient
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy.streaming import StreamListener
 
 from opsdroid.connector import Connector
 from opsdroid.message import Message
 
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class StdOutListener(StreamListener):
+
+    def __init__(self, opsdroid, connector):
+        self.tweetCount = 0
+        self.opsdroid = opsdroid
+        self.connector = connector
+
+    def on_connect(self):
+        _LOGGER.debug("Connection established!!")
+
+    def on_disconnect(self, notice):
+        _LOGGER.debug("Connection lost!! : ", notice)
+
+    def on_data(self, status):
+        _LOGGER.debug("Processing tweet.")
+        status = json.loads(status)
+        if self.connector.config.get("enable_dms", False) and \
+                "direct_message" in status and \
+                "sender" in status["direct_message"] and \
+                status["direct_message"]["sender"]["screen_name"] != self.connector.screen_name:
+            self.connector.process_dm(status["direct_message"])
+
+        if self.connector.config.get("enable_tweets", False) and \
+                "created_at" in status and \
+                "id" in status and \
+                "text" in status and \
+                "user" in status and \
+                status["user"]["screen_name"] != self.connector.screen_name:
+            self.connector.process_tweet(status)
+
+        return True
+
+    def on_error(self, status):
+        print(status)
 
 
 class ConnectorTwitter(Connector):
@@ -18,57 +56,70 @@ class ConnectorTwitter(Connector):
         logging.debug("Loaded twitter connector")
         super().__init__(config)
         self.name = "twitter"
-        self.latest_dm_update = None
+        self.screen_name = None
         self.config = config
+        self.opsdroid = None
         try:
-            self.client = TwitterClient(
-                consumer_key=self.config["consumer_key"],
-                consumer_secret=self.config["consumer_secret"],
-                oauth_token=self.config["oauth_token"],
-                oauth_token_secret=self.config["oauth_token_secret"],
-            )
+            self.auth = OAuthHandler(self.config["consumer_key"],
+                                     self.config["consumer_secret"])
+            self.auth.secure = True
+            self.auth.set_access_token(self.config["oauth_token"],
+                                       self.config["oauth_token_secret"])
         except KeyError as e:
             _LOGGER.error("Missing auth tokens!")
 
     async def connect(self, opsdroid):
         """Connect to twitter."""
-        logging.debug("Connecting to twitter")
+        _LOGGER.debug("Connecting to twitter")
+        self.opsdroid = opsdroid
+        self.api = API(self.auth)
+        self.screen_name = self.api.me().name
+        _LOGGER.debug(self.screen_name)
 
     async def listen(self, opsdroid):
         """Listen for new message."""
-        while True:
-            if self.config.get("enable_dms", True):
-                await self._listen_dms(opsdroid)
+        self.stream = Stream(self.auth, StdOutListener(opsdroid, self))
+        opsdroid.eventloop.run_in_executor(None, self.stream.userstream)
 
-    async def _listen_dms(self, opsdroid):
-        if self.latest_dm_update is not None:
-            params = {"since_id": self.latest_dm_update}
-        else:
-            params = {}
-        resp = await self.client.request(
-            'GET', 'direct_messages.json', params=params)
-        direct_messages = await resp.json()
-        resp.release()
-        for direct_message in direct_messages:
-            _LOGGER.debug(direct_message)
-            if self.latest_dm_update is None or self.latest_dm_update < direct_message["id"]:
-                self.latest_dm_update = direct_message["id"]
-            message = Message(direct_message["text"],
-                              direct_message["sender_id"],
-                              "dm",
-                              self)
-            await opsdroid.parse(message)
-        await asyncio.sleep(60)  # Requests limited to 15 per 15 min window
+    def clean_tweet(self, tweet):
+        return tweet.replace("@{}".format(self.screen_name), '').strip()
+
+    def process_tweet(self, tweet):
+        """Create a Message from a tweet json and parse it."""
+        _LOGGER.debug("Processing tweet")
+        _LOGGER.debug(tweet)
+        tweet["text"] = self.clean_tweet(tweet["text"])
+        message = Message(tweet["text"],
+                          tweet["user"]["screen_name"],
+                          {"type": "tweet", "id": tweet["id"]},
+                          self)
+        self.opsdroid.eventloop.create_task(self.opsdroid.parse(message))
+
+    def process_dm(self, dm):
+        """Create a Message from a tweet json and parse it."""
+        _LOGGER.debug("Processing direct message")
+        _LOGGER.debug(dm)
+        message = Message(dm["text"],
+                          dm["sender"]["screen_name"],
+                          {"type": "dm", "id": dm["id"]},
+                          self)
+        self.opsdroid.eventloop.create_task(self.opsdroid.parse(message))
 
     async def respond(self, message):
         """Respond with a message."""
-        if message.room == "dm" and self.config.get("enable_dms", True):
-            await self._respond_dm(message)
-
-    async def _respond_dm(self, message):
-        logging.debug("Responding with: " + message.text)
-        params = {"user_id": message.user,
-                  "text": message.text}
-        resp = await self.client.request(
-            'POST', 'direct_messages/new.json', params=params)
-        resp.release()
+        _LOGGER.debug("Responding via twitter")
+        _LOGGER.debug(message.user)
+        _LOGGER.debug(message.text)
+        if message.room["type"] == "dm":
+            self.api.send_direct_message(screen_name=message.user,
+                                         text=message.text)
+        if message.room["type"] == "tweet":
+            if message.user is None:
+                self.api.update_status(status=message.text)
+            else:
+                if "id" in message.room:
+                    self.api.update_status(status=message.text,
+                                           in_reply_to_status_id=message.room["id"])
+                else:
+                    tweet = "@{} {}".format(message.user, message.text)
+                    self.api.update_status(status=tweet)
